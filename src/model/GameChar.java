ffe9624dcfd06e3199e09cf4ed9191d86f165eba
Rodolfo Rodrigues <rodolfo.raar@gmail.com>
2014-06-04 16:30:54 +0100

    GameChar - Interface updated

--- src/model/GameChar.java
+++ src/model/GameChar.java
@@ -3,11 +3,39 @@ package model;
 import model.board.*;
 
 public interface GameChar {
+	
+	/**
+	 * Monster visits bomb control item in game board
+	 */
 	public abstract void visitBombControl(BombControl item);
+	
+	/**
+	 * Monster visits extra bomb item in game board
+	 */
 	public abstract void visitExtraBomb(ExtraBomb item);
+	
+	/**
+	 * Monster visits bomb power up item in game board
+	 */
 	public abstract void visitBombPowerUp(BombPowerUp item);
+	
+	/**
+	 * Monster visits boost speed item in game board
+	 */
 	public abstract void visitBoostSpeed(BoostSpeed item);
+	
+	/**
+	 * Monster visits board exit item in game board
+	 */
 	public abstract void visitBoardExit(BoardExit item);
+	
+	/**
+	 * Monster visits path item in game board
+	 */
 	public abstract void visitPath(Path item);
+	
+	/**
+	 * Monster visits undestructible wall item in game board
+	 */
 	public abstract void visitUndestructibleWall(UndestructibleWall item);
 }
