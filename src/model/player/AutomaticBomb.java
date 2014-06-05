668a82a9cfa6e0c62d5dbc991b74206ff6c37544
Rodolfo Rodrigues <rodolfo.raar@gmail.com>
2014-06-05 17:51:39 +0100

    Automatic Bomb - Timer implemented

--- src/model/player/AutomaticBomb.java
+++ src/model/player/AutomaticBomb.java
@@ -1,6 +1,13 @@
 package model.player;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.Timer;
+
+import model.GameModel;
 import model.Position;
+
 
 /**
  * The Class AutomaticBomb.
@@ -12,9 +19,9 @@ public class AutomaticBomb implements Bomb {
 	
 	/** The board position where the bomb was dropped */
 	private Position boardPosition;
-
-	/** The time out. */
-	private int timeOut = 5;
+	
+	/** The bomb timer */
+	private Timer bombTimer;
 	
 	/** The animation for the bomb */
 	//private Animation animation;
@@ -27,6 +34,15 @@ public class AutomaticBomb implements Bomb {
 	public AutomaticBomb(Position dropPos){
 		this.range = 3;
 		this.boardPosition = dropPos;
+		
+		ActionListener bombTimerListener = new ActionListener(){ 
+			public void actionPerformed(ActionEvent e) {
+				detonate();
+				bombTimer.stop();
+			}
+		};
+				
+		bombTimer = new Timer(1000, bombTimerListener);	
 	}
 	
 	/**
@@ -34,7 +50,10 @@ public class AutomaticBomb implements Bomb {
 	 * was dropped and the cells got in the bomb range. This object is destroyed.
 	 * 
 	 */
-	public void detonate(){}
+	public void detonate(){
+		
+		GameModel.getInstance().getBoard().getItem(boardPosition).explode();
+	}
 
 	/**
 	 * Draw.
