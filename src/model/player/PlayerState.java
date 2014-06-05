599b1537c3291d369a5dd9d51259bc6704e7e8d4
Rodolfo Rodrigues <rodolfo.raar@gmail.com>
2014-06-05 10:30:58 +0100

    PlayerState - Interface updated with new return values

--- src/model/player/PlayerState.java
+++ src/model/player/PlayerState.java
@@ -5,92 +5,33 @@ package model.player;
  * This class defines an interface for encapsulating the behaviour associated with
  * a particular state of the Player.
  */
-public abstract class PlayerState {
-
-	/** The player. */
-	private Player player;
-	
-	/** The draw rate. */
-	private int drawRate;
-
-	/**
-	 * Instantiates a new player state.
-	 */
-	public PlayerState(){
-
-	}
-
-	/* (non-Javadoc)
-	 * @see java.lang.Object#finalize()
-	 */
-	public void finalize() throws Throwable {
-
-	}
-	
-	/**
-	 * Move.
-	 */
-	public void move(){
-		//player->updateBoardPosition
-
-
-	}
+public interface PlayerState {
 
 	/**
 	 * Turn up.
 	 */
-	public void turnUp(){
-
-	}
+	public abstract PlayerState turnUp();
 
 	/**
 	 * Turn left.
 	 */
-	public void turnLeft(){
-
-	}
+	public abstract PlayerState turnLeft();
 
 	/**
 	 * Turn down.
 	 */
-	public void turnDown(){
-
-	}
+	public abstract PlayerState turnDown();
 
 	/**
 	 * Turn right.
 	 */
-	public void turnRight(){
-
-	}
+	public abstract PlayerState turnRight();
 
 	/**
 	 * Die.
 	 */
-	public void die(){
+	public abstract PlayerState die();
 
-	}
+}
 
-	/**
-	 * Respawn.
-	 */
-	public void respawn(){
-
-	}
-
-	/**
-	 * Update.
-	 */
-	public void update(){
-
-	}
-
-	/**
-	 * Update draw rate.
-	 *
-	 * @param rate the rate
-	 */
-	public void updateDrawRate(int rate){
-
-	}
-}//end PlayerState
+//end PlayerState
