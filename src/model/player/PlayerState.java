e1238ee6e1ccfff91cf965e8e4a695247245ed70
Rodolfo Rodrigues <rodolfo.raar@gmail.com>
2014-06-05 14:12:19 +0100

    PlayerState - generateNextMov() added

--- src/model/player/PlayerState.java
+++ src/model/player/PlayerState.java
@@ -1,10 +1,18 @@
 package model.player;
 
+import model.Position;
+
 /**
  * This class defines an interface for encapsulating the behaviour associated with
  * a particular state of the Player.
  */
 public interface PlayerState {
+
+	//Player Movement Increments
+	public static final Position UP = new Position(-1,0);
+	public static final Position DOWN = new Position(1,0);
+	public static final Position LEFT = new Position(0,-1);
+	public static final Position RIGHT = new Position(0,1);
 	
 	/**
 	 * Turn up.
@@ -35,6 +43,14 @@ public interface PlayerState {
 	 * Die.
 	 */
 	public abstract PlayerState die();
+	
+	/**
+	 * Generate player next movement
+	 * 
+	 * @param player moving
+	 * @return newPosPlayer
+	 */
+	public abstract Position generateNextMov(Player playerMoving);
 
 }
 
