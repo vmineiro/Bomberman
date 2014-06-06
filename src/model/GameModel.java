ee5e5e6f77daeb3eb50af57d547b2b4d144fa8ac
Rodolfo Rodrigues <rodolfo.raar@gmail.com>
2014-06-06 14:56:20 +0100

    GameModel - inc player movement fixed

--- src/model/GameModel.java
+++ src/model/GameModel.java
@@ -56,10 +56,10 @@ public class GameModel {
 	private boolean pressedRight = false;
 	
 	//Player Movement Increments
-	public static final Position UP = new Position(0,-1);
-	public static final Position DOWN = new Position(0,1);
-	public static final Position LEFT = new Position(-1,0);
-	public static final Position RIGHT = new Position(1,0);
+	public static final Position UP = new Position(-1,0);
+	public static final Position DOWN = new Position(1,0);
+	public static final Position LEFT = new Position(0,-1);
+	public static final Position RIGHT = new Position(0,1);
 	
 	// =====================================================================
 
