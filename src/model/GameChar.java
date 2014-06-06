29c7050908433f5c6aaaa551499e7dd028cc64ab
Rodolfo Rodrigues <rodolfo.raar@gmail.com>
2014-06-06 16:53:06 +0100

    CheckCollisions added

--- src/model/GameChar.java
+++ src/model/GameChar.java
@@ -1,8 +1,28 @@
 package model;
 
 import model.board.*;
+import model.monster.Monster;
+import model.player.Player;
 
 public interface GameChar {
+	
+	/**
+	 * Collision Management
+	 * @param gameChar
+	 */
+	public abstract void collidesWith(GameChar gameChar);
+	
+	/**
+	 * Visited by Player
+	 * @param vPlayer
+	 */
+	public abstract void visit(Player vPlayer);
+	
+	/**
+	 * Visited by Monster
+	 * @param vMonster
+	 */
+	public abstract void visit(Monster vMonster);
 	
 	/**
 	 * Monster visits bomb control item in game board
