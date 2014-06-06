d2eb64f941df506096c46d91db672c2101dcf9a5
Rodolfo Rodrigues <rodolfo.raar@gmail.com>
2014-06-06 15:23:40 +0100

    UndestrucWall - draw() added

--- src/model/board/UndestructibleWall.java
+++ src/model/board/UndestructibleWall.java
@@ -1,5 +1,6 @@
 package model.board;
 
+import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -79,6 +80,14 @@ public class UndestructibleWall extends Item {
 		this.state = state;
 		setAnimation(steelImg);
 		
+	}
+	
+	/**
+	 * Draw undestructible Wall
+	 */
+	public void draw(Graphics g, int pos_l, int pos_c, int width, int height)
+	{
+		g.drawImage(steelImg, pos_c*width, pos_l*height, (pos_c*width)+width, (pos_l*height)+height, 0, 0, 512, 512, null);
 	}
 	
 
