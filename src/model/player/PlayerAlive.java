0eb6f1eca95cbe64ba9c5b06238454c21693d550
Rodolfo Rodrigues <rodolfo.raar@gmail.com>
2014-06-06 03:31:18 +0100

    PlayerAlive - incMovement fixed

--- src/model/player/PlayerAlive.java
+++ src/model/player/PlayerAlive.java
@@ -5,9 +5,9 @@ import model.Position;
 public interface PlayerAlive extends PlayerState{
 	
 	//Player Movement Increments
-	public static final Position UP = new Position(-1,0);
-	public static final Position DOWN = new Position(1,0);
-	public static final Position LEFT = new Position(0,-1);
-	public static final Position RIGHT = new Position(0,1);
+	public static final Position UP = new Position(0,-1);
+	public static final Position DOWN = new Position(0,1);
+	public static final Position LEFT = new Position(-1,0);
+	public static final Position RIGHT = new Position(1,0);
 	
 }
