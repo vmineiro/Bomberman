3cdf3e3b19481e1e9d4f99e9826ff23d262d21da
Rodolfo Rodrigues <rodolfo.raar@gmail.com>
2014-06-08 02:12:56 +0100

    Monster Tests updated

--- src/tests/MonsterTests.java
+++ src/tests/MonsterTests.java
@@ -1,11 +1,17 @@
 package tests;
 
+import model.GameModel;
 import model.Position;
+import model.board.BoardExit;
 import model.board.ItemExploding;
 import model.board.ItemPath;
+import model.board.UndestructibleWall;
 import model.monster.Monster;
 import model.monster.MonsterAlive;
 import model.monster.MonsterDead;
+import model.player.Player;
+import model.player.PlayerAlive;
+import model.player.PlayerDead;
 
 import org.junit.Test;
 
@@ -25,41 +31,48 @@ public class MonsterTests {
 	@Test
 	public void moveTests(){
 		
-		//TODO: Improve moveTests
-		Monster monster_T = new Monster();
+		Monster monster_t = new Monster();
 
-		assertNotNull(monster_T);
-		assertNotNull(monster_T.getBoardPosition());
-		assertNotNull(monster_T.getCurrentState());
+		assertNotNull(monster_t);
+		assertNotNull(monster_t.getBoardPosition());
+		assertNotNull(monster_t.getCurrentState());
 
 		// Set monster start position in (3,3)
-		monster_T.setBoardPosition(monster_T.getBoardPosition().add(new Position(3,3)));
-		// Generates randomly monster next position
-		monster_T.setBoardPosition(monster_T.generateNextMov());
-		//Verifies monster next position
-		assertTrue(monster_T.getBoardPosition().equals(new Position(4, 3)) || monster_T.getBoardPosition().equals(new Position(2, 3)) 
-				|| monster_T.getBoardPosition().equals(new Position(3, 4)) || monster_T.getBoardPosition().equals(new Position(3, 2)));
-	}
-
-	/**
-	 * Power up tests.
-	 * 
-	 * Neste teste pretende-se testar as alteracoes de estado dos tipos de monstros,
-	 * quando visitam uma casa de PowerUp. 
-	 */
-	@Test
-	public void powerUpTests(){
+		monster_t.setBoardPosition(monster_t.getBoardPosition().add(new Position(3,3)));
 		
+		// Generates randomly monster next position
+		monster_t.setBoardPosition(monster_t.generateNextMov());
+		
+		//Verifies monster next position
+		assertTrue(monster_t.getBoardPosition().equals(new Position(4, 3)) || monster_t.getBoardPosition().equals(new Position(2, 3)) 
+				|| monster_t.getBoardPosition().equals(new Position(3, 4)) || monster_t.getBoardPosition().equals(new Position(3, 2)));
+		
+		//Check Collision Test
+		Position pos_t = new Position(3,3);
+		GameModel.getInstance().getPlayers().updateBoardPosition(pos_t);
+		
+		//Monster is Dead and in a different position
+		monster_t.setBoardPosition(new Position(0, 0));
+		monster_t.setCurrentState(new MonsterDead());
+		assertFalse("Monster is Dead - Collision",monster_t.checkCollision());
+		
+		//Monster is Alive and in a different position
+		monster_t.setCurrentState(new MonsterAlive());
+		assertFalse("Monster is Alive - Collision",monster_t.checkCollision());
+		
+		//Monster is Alive and in a same position
+		monster_t.setBoardPosition(pos_t);
+		assertTrue("Monster is Alive and same position - Collision",monster_t.checkCollision());
 	}
 
 	/**
 	 * Death test.
 	 * 
-	 * Neste teste pretende-se testar a morte dos montros. 
+	 * Neste teste pretende-se testar
 	 */
 	@Test
 	public void deathTest(){
-		//TODO: Improve deathTests
+
 		Monster monster_T = new Monster();
 
 		assertNotNull(monster_T);
@@ -67,12 +80,40 @@ public class MonsterTests {
 		assertNotNull(monster_T.getCurrentState());
 		
 		// Verify alive state
-		assertEquals("Monster is Alive", MonsterAlive.class, monster_T.getCurrentState().getClass());
+		assertEquals("Monster is Alive - State", MonsterAlive.class, monster_T.getCurrentState().getClass());
 		
 		monster_T.setCurrentState(monster_T.getCurrentState().die());
 		
 		//Verify dead state
-		assertEquals("Monster is Dead", MonsterDead.class, monster_T.getCurrentState().getClass());
+		assertEquals("Monster is Dead - State", MonsterDead.class, monster_T.getCurrentState().getClass());	
+	}
+	
+	/**
+	 * Visit by Player Tests
+	 * 
+	 * Avaliar a visita do player com diferentes estados dos monstros
+	 * 
+	 */
+	@Test
+	public void playerVisitTests(){
+
+		Monster monster_T = new Monster();
+
+		//Visit with player - Monster Dead
+		Player player_t = new Player();
+		monster_T.visit(player_t);
+
+		//Verify alive state
+		assertEquals("Monster is Alive - Visit", MonsterAlive.class, monster_T.getCurrentState().getClass());
+		assertEquals("Player is Dead - Visit", PlayerDead.class, player_t.getCurrentState().getClass());
+		
+		monster_T.setCurrentState(new MonsterDead());
+		player_t.setCurrentState(new PlayerAlive());
+		monster_T.visit(player_t);
+
+		//Verify dead state
+		assertEquals("Monster is Dead - Visit", MonsterDead.class, monster_T.getCurrentState().getClass());
+		assertEquals("Player is Alive - Visit", PlayerAlive.class, player_t.getCurrentState().getClass());		
 	}
 	
 	/**
@@ -82,7 +123,7 @@ public class MonsterTests {
 	 */
 	@Test
 	public void deathDetonationTests(){
-		//TODO: Improve deathDetonationTests
+		
 		Monster monster_t = new Monster();
 
 		//Testing Check Death
@@ -91,7 +132,56 @@ public class MonsterTests {
 		ItemPath path_t = new ItemPath();
 		path_t.setCurrentState(new ItemExploding());
 		monster_t.checkDeath(path_t);
+		
 		assertEquals("Monster is Dead after Explosion", MonsterDead.class, monster_t.getCurrentState().getClass());
+	}
+	
+	/**
+	 * Items visit by monster Tests
+	 * Nestes testes pretende-se verificar o comportamento do monstro nos varios items do tabuleiro
+	 */
+	@Test
+	public void itemsVisitTests(){
+		
+		Monster monster_t = new Monster();
+		
+		ItemPath path_t = new ItemPath();
+		UndestructibleWall uw_t = new UndestructibleWall();
+		BoardExit be_t = new BoardExit();
+		
+		monster_t.visitBoardExit(be_t);
+		
+		//Verify alive state
+		assertEquals("Monster is Alive - Board Exit", MonsterAlive.class, monster_t.getCurrentState().getClass());
+		
+		monster_t.visitPath(path_t);
+		
+		//Verify alive state
+		assertEquals("Monster is Alive - Path", MonsterAlive.class, monster_t.getCurrentState().getClass());
+		
+		monster_t.visitUndestructibleWall(uw_t);
+		
+		//Verify alive state
+		assertEquals("Monster is Alive - Undestructible Wall", MonsterAlive.class, monster_t.getCurrentState().getClass());
+		
+		be_t.setCurrentState(new ItemExploding());
+		path_t.setCurrentState(new ItemExploding());
+		uw_t.setCurrentState(new ItemExploding());
+		
+		monster_t.visitBoardExit(be_t);
+		
+		//Verify alive state
+		assertEquals("Monster is Dead - Board Exit", MonsterDead.class, monster_t.getCurrentState().getClass());
+		
+		monster_t.visitPath(path_t);
+		
+		//Verify alive state
+		assertEquals("Monster is Dead - Path", MonsterDead.class, monster_t.getCurrentState().getClass());
+		
+		monster_t.visitUndestructibleWall(uw_t);
+		
+		//Verify alive state
+		assertEquals("Monster is Dead - Undestructible Wall", MonsterDead.class, monster_t.getCurrentState().getClass());
 	}
 	
 }
